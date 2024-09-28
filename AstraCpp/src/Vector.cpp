a2cc7914b024c44d54980ec1cbad1b0d2dd14bb6
Dr-Lepic <mbmahbub007@gmail.com>
2024-09-28 20:02:30 +0600

    Update Vector.cpp

--- AstraCpp/src/Vector.cpp
+++ AstraCpp/src/Vector.cpp
@@ -1,9 +1,13 @@
 #include "pch.h"
 #include "Vector.h"
+#include <stdexcept>
 
 using namespace astra;
 
 Vector::Vector(int size) {
+    if (size < 0) {
+        throw std::invalid_argument("Size must be non-negative");
+    }
     this->size = size;
     this->values = new double[size];
 }
