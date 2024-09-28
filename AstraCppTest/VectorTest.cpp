1975dd1d93c2d08dafb46c91a42597feaeab0c0b
SillyCatto <raiyanmuhtasim@gmail.com>
2024-09-28 23:30:28 +0600

    wrote test for vector array initialization

--- AstraCppTest/VectorTest.cpp
+++ AstraCppTest/VectorTest.cpp
@@ -3,16 +3,14 @@
 #include "Vector.h"
 #include "gtest/gtest.h"
 
-using namespace astra;
+namespace astra {
 
 // Test fixture class for Vector
 class VectorTest : public ::testing::Test {
   protected:
     Vector* vec;
 
-    void SetUp() override {
-        vec = new Vector(3);
-    }
+    void SetUp() override { vec = new Vector(3); }
 
     void TearDown() override {
         // Clean up after each test
@@ -20,8 +18,15 @@ class VectorTest : public ::testing::Test {
     }
 };
 
-
 TEST_F(VectorTest, size_initialization) {
     Vector v(5);
     EXPECT_EQ(v.getSize(), 5);
 }
+
+TEST_F(VectorTest, array_initialization) {
+    double vals[] = {1.0, 2.0, 3.0};
+    Vector v(vals, 3);
+    EXPECT_EQ(v.getSize(), 3);
+}
+
+} // namespace astra
