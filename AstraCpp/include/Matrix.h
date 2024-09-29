ca8d10b229c905e4c312c800a1112f193e2294d1
SillyCatto <raiyanmuhtasim@gmail.com>
2024-09-29 21:21:14 +0600

    implement minimum skeleton for Matrix class

--- AstraCpp/include/Matrix.h
+++ AstraCpp/include/Matrix.h
@@ -3,6 +3,15 @@
 namespace astra {
 class Matrix {
   private:
+    int rows;
+    int cols;
+    double* values;
+
   public:
+    Matrix(int r, int c);
+    ~Matrix();
+
+    int get_row() const;
+    int get_col() const;
 };
-}
+} // namespace astra
--- AstraCpp/src/Matrix.cpp
+++ AstraCpp/src/Matrix.cpp
@@ -1,2 +1,11 @@
 #include "pch.h"
 #include "Matrix.h"
+
+using namespace astra;
+
+Matrix::Matrix(int r, int c) : rows(r), cols(c) { values = new double[r * c]; }
+
+Matrix::~Matrix() { delete[] values; }
+
+int Matrix::get_row() const { return rows; }
+int Matrix::get_col() const { return cols; }
