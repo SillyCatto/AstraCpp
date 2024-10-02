3a466eefa91d5136d2de225196296afe4729d31c
SillyCatto <raiyanmuhtasim@gmail.com>
2024-10-02 19:49:58 +0600

    added test for valid and invalid matrix creation

--- AstraCppTest/MatrixTest.cpp
+++ AstraCppTest/MatrixTest.cpp
@@ -2,7 +2,8 @@
 
 #include "Matrix.h"
 #include <gtest/gtest.h>
-#include <stdexcept>
+
+namespace astra {
 
 // Test fixture for Matrix class
 class MatrixTest : public ::testing::Test {
@@ -11,3 +12,19 @@ class MatrixTest : public ::testing::Test {
 
     void TearDown() override {}
 };
+
+
+TEST_F(MatrixTest, valid_matrix_creation) {
+    Matrix m(2, 3);
+    EXPECT_EQ(m.get_row(), 2);
+    EXPECT_EQ(m.get_col(), 3);
+}
+
+
+TEST_F(MatrixTest, invalid_matrix_creation) {
+    EXPECT_THROW(Matrix(-2, 3), std::invalid_argument);
+    EXPECT_THROW(Matrix(2, 0), std::invalid_argument);
+    EXPECT_THROW(Matrix(0, 0), std::invalid_argument);
+}
+
+} // namespace astra
