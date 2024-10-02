54f5dbe4c103410a6722a220e77376a9edfdf80e
saadthenexus <hasibulkarimsaad@gmail.com>
2024-10-02 18:28:04 +0600

    Added access elements test

--- AstraCppTest/VectorTest.cpp
+++ AstraCppTest/VectorTest.cpp
@@ -56,4 +56,13 @@ TEST_F(VectorTest, comma_initializer) {
     EXPECT_EQ(v[2], 5);
 }
 
+TEST_F(VectorTest, access_elements) {
+    double arr[] = {1.0, 2.0, 3.0};
+    Vector v(arr, 3);
+
+    EXPECT_EQ(v[0], 1.0);
+    EXPECT_EQ(v[1], 2.0);
+    EXPECT_EQ(v[2], 3.0);
+}
+
 } // namespace astra
