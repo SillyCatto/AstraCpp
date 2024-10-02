80fe4958f4bac350c76509192f92f074054aca2d
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-02 19:20:21 +0600

    Tested Vector dot for invalid size

--- AstraCppTest/VectorTest.cpp
+++ AstraCppTest/VectorTest.cpp
@@ -136,4 +136,13 @@ TEST_F(VectorTest, vector_dot) {
     EXPECT_EQ(result[2], 18.0);
 }
 
+TEST_F(VectorTest, vector_dot_invalid_size) {
+    double arr1[] = {1.0, 2.0, 3.0};
+    double arr2[] = {4.0, 5.0};
+    Vector v1(arr1, 3);
+    Vector v2(arr2, 2);
+
+    EXPECT_THROW(v1 * v2, std::invalid_argument);
+}
+
 } // namespace astra
