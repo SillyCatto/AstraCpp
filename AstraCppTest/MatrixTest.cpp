cdcfe8da1d1707b56db2afa605f416dedc89fcf3
SillyCatto <raiyanmuhtasim@gmail.com>
2024-10-02 19:47:06 +0600

    added test fixture class for matrix test

--- AstraCppTest/MatrixTest.cpp
+++ AstraCppTest/MatrixTest.cpp
@@ -1,1 +1,13 @@
 #include "pch.h"
+
+#include "Matrix.h"
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+// Test fixture for Matrix class
+class MatrixTest : public ::testing::Test {
+  protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
