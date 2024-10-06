305e11929163bfd37886dd1a7152aac566ce65cc
SillyCatto <raiyanmuhtasim@gmail.com>
2024-10-06 23:08:51 +0600

    modified invalid_size exception for better clarity

--- AstraCpp/internals/Exceptions.h
+++ AstraCpp/internals/Exceptions.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <exception>
+#include <string>
 
 namespace astra::internals::exceptions {
 
-class invalid_vector_size : public std::exception {
+class invalid_size : public std::exception {
+  private:
+    std::string msg;
 
   public:
-    const char* what() const noexcept override {
-        return "[ERROR]  vector size must be positive";
+    invalid_size(const std::string& type, 
+                 const char* file = __FILE__, int line = __LINE__) {
+        msg = "[ASTRA] invalid size for " + type +
+                  " initialization. (File: " + file +
+                  ", Line: " + std::to_string(line) + ")";
     }
+
+    const char* what() const override { return msg.c_str(); }
 };
 }  // namespace astra::internals::exceptions
