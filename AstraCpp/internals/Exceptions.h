ba882853c182f17052606bed4c851261f0953498
SillyCatto <raiyanmuhtasim@gmail.com>
2024-10-06 21:38:08 +0600

    addedd  invalid vector size exception

--- AstraCpp/internals/Exceptions.h
+++ AstraCpp/internals/Exceptions.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <exception>
 
 namespace astra::internals::exceptions {
 
+class invalid_vector_size : public std::exception {
+
+  public:
+    const char* what() const noexcept override {
+        return "[ERROR]  vector size must be positive";
+    }
+};
 }  // namespace astra::internals::exceptions
