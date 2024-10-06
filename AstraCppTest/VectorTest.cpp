64e27694cb881a8c79bb3fd712565b717447d33a
Dr-Lepic <mbmahbub007@gmail.com>
2024-10-06 18:34:31 +0600

    Tested scalar multiplication for negative num

--- AstraCppTest/VectorTest.cpp
+++ AstraCppTest/VectorTest.cpp
@@ -155,6 +155,17 @@ TEST_F(VectorTest, scalar_multiplication_positive) {
     EXPECT_EQ(result[1], 4.0);
     EXPECT_EQ(result[2], 6.0);
 }
+
+TEST_F(VectorTest, scalar_multiplication_negative) {
+    double arr[] = {1.0, 2.0, 3.0};
+    Vector v(arr, 3);
+
+    Vector result = v * -2;
+
+    EXPECT_EQ(result[0], -2.0);
+    EXPECT_EQ(result[1], -4.0);
+    EXPECT_EQ(result[2], -6.0);
+}
   
 TEST_F(VectorTest, copy_assignment_deep_copy) {
     double arr1[] = {1.0, 2.0, 3.0};
