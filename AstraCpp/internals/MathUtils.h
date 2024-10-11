dabd6d4fbeedbf8e898e488edbe4cb35764bbc7a
saadthenexus <hasibulkarimsaad@gmail.com>
2024-10-11 21:06:12 +0600

    Made a custom sqrt function

--- AstraCpp/internals/MathUtils.h
+++ AstraCpp/internals/MathUtils.h
@@ -4,4 +4,25 @@ namespace astra::internals::mathutils {
 
 	inline double abs(double x) { return (x < 0) ? -x : x; }
 
+	inline double sqrt(double n) {
+        if (n < 0) {
+            throw std::invalid_argument("Cannot compute the square root of a negative number");
+        }
+        if (n == 0) {
+            return 0;
+        }
+            
+        double tolerance = 1e-9;
+        double guess = n;
+        double previous_guess;
+
+        do {
+            previous_guess = guess;
+            guess = 0.5 * (guess + n / guess); 
+        } while (abs(guess - previous_guess) >
+                     tolerance); 
+
+        return guess;
+     }
+
 } // namespace astra::internals::mathutils
