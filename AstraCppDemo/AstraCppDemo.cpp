2f14196f52cd90732917303b5df10a159d6f8ccd
saadthenexus <hasibulkarimsaad@gmail.com>
2024-11-04 18:48:24 +0600

    Demo vector creation

--- AstraCppDemo/AstraCppDemo.cpp
+++ AstraCppDemo/AstraCppDemo.cpp
@@ -21,6 +21,21 @@ int main() {
 
     std::cout << "\n\n";
 
+    double values1[] = {3, 4, 5};
+    double values2[] = {1, 2, 3};
+
+    astra::Vector vec1(3, values1);
+    astra::Vector vec2(3, values2);
+
+    astra::Vector vec3(3);
+    vec3 << 1 << 2 << 3;
+
+    std::cout << "Vector1: " << vec1 << "\n"
+              << "Vector2: " << vec2 << "\n"
+              << "Vector3: " << vec3 << std::endl; 
+
+
+
 
     // ----------MATRIX----------
     astra::Matrix m(3, 3);
