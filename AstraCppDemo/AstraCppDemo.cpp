68d4034b3d9efd4d0bb314e52642dedd1e1636ed
SillyCatto <raiyanmuhtasim@gmail.com>
2024-11-04 23:26:40 +0600

    cleaned up the demo

--- AstraCppDemo/AstraCppDemo.cpp
+++ AstraCppDemo/AstraCppDemo.cpp
@@ -5,19 +5,6 @@
 
 int main() {
     // ----------VECTOR----------
-    double arr[] = {1, 0, 0};
-    astra::Vector v1(3, arr);
-
-    astra::Vector v2(3);
-    v2 << 0, 1, 0;
-
-    astra::Vector v3(3);
-
-
-    std::cout << astra::Vector::angle_deg(v1, v2) << "\n";
-
-    v3 = v1 / 3;
-    std::cout << v3 << "\n";
 
     std::cout << "\n\n";
 
@@ -28,11 +15,11 @@ int main() {
     astra::Vector vec2(3, values2);
 
     astra::Vector vec3(3);
-    vec3 << 1 << 2 << 3;
+    vec3 << 1, 2, 3;
 
     std::cout << "vec1: " << vec1 << "\n"
               << "vec2: " << vec2 << "\n"
-              << "vec3: " << vec3 << "\n"; 
+              << "vec3: " << vec3 << "\n\n"; 
 
     astra::Vector additionResult = vec1 + vec2;
     std::cout << "Addition (vec1 + vec2): " << additionResult << "\n";
@@ -45,22 +32,25 @@ int main() {
               << scalarMultiplicationResult << "\n";
 
     astra::Vector scalarDivisionResult = vec1 / 2.0;
-    std::cout << "Scalar Division (vec1 / 2): " << scalarDivisionResult << "\n";
+    std::cout << "Scalar Division (vec1 / 2): " << scalarDivisionResult << "\n\n";
+
+    double dotProductResult = vec1 * vec2;
+    std::cout << "Dot Product (vec1 * vec2): " << dotProductResult<< "\n";
 
     astra::Vector crossProductResult = vec1 ^ vec2;
-    std::cout << "Cross Product (vec1 ^ vec2): " << crossProductResult << "\n";
+    std::cout << "Cross Product (vec1 ^ vec2): " << crossProductResult << "\n\n";
 
     double magnitudeVec1 = vec1.magnitude();
     std::cout << "Magnitude of vec1: " << magnitudeVec1 << "\n";
 
     astra::Vector normalizedVec1 = vec1.normalize();
-    std::cout << "Normalization of vec1: " << normalizedVec1 << "\n";
+    std::cout << "Normalization of vec1: " << normalizedVec1 << "\n\n";
 
     double angleRad = astra::Vector::angle(vec1, vec2);
     std::cout << "Angle between vec1 and vec2 (radians): " << angleRad << "\n";
 
     double angleDeg = astra::Vector::angle_deg(vec1, vec2);
-    std::cout << "Angle between vec1 and vec2 (degrees): " << angleDeg << "\n";
+    std::cout << "Angle between vec1 and vec2 (degrees): " << angleDeg << "\n\n";
 
     std::cout << "Sum of vec1: " << vec1.sum() << "\n";
     std::cout << "Average of vec1: " << vec1.avg() << "\n";
@@ -69,22 +59,8 @@ int main() {
 
     std::cout << "vec1 == vec2? " << (vec1 == vec2 ? "Yes" : "No") << "\n";
 
-    astra::Vector vec4(3);
-    vec4 << 5, 6, 7; 
-    std::cout << "vec4 initialized with << and , operators: " << vec4 << "\n";
 
     // ----------MATRIX----------
-    astra::Matrix m(3, 3);
-
-    /*m(0, 0) = 1;
-    m(0, 1) = 2;
-    m(1, 0) = 3;
-    m(1, 1) = 4;*/
-
-    m << 12.34,   2, 7,
-             4, 5.5, 9;
-
-    m.print();
 
     std::cin.get();
 
