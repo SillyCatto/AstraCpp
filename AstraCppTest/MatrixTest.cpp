65192266dad11b61ff26b259e67510d8ccf19cf2
saadthenexus <hasibulkarimsaad@gmail.com>
2024-11-10 21:56:30 +0600

    Added test cases for  <<, (shouldve done earlier but still doing it now xD)

--- AstraCppTest/MatrixTest.cpp
+++ AstraCppTest/MatrixTest.cpp
@@ -43,6 +43,28 @@ TEST_F(MatrixTest, comma_initializer) {
     EXPECT_EQ(m(1, 1), 4.0);
 }
 
+TEST_F(MatrixTest, InsertValidValue) {
+    Matrix mat(2, 2);
+    mat << 1 << 2 << 3 << 4;
+    EXPECT_EQ(mat(0, 0), 1);
+    EXPECT_EQ(mat(1, 1), 4);
+}
+
+TEST_F(MatrixTest, InsertExceedingMatrixSize) {
+    Matrix mat(2, 2);
+    EXPECT_THROW(mat << 1 << 2 << 3 << 4 << 5,
+                 astra::internals::exceptions::init_out_of_range);
+}
+
+TEST_F(MatrixTest, InsertPartialFill) {
+    Matrix mat(2, 2);
+    mat << 1 << 2;
+    EXPECT_EQ(mat(0, 0), 1);
+    EXPECT_EQ(mat(0, 1), 2);
+    EXPECT_EQ(mat(1, 0), 0);
+    EXPECT_EQ(mat(1, 1), 0); 
+}
+
 TEST_F(MatrixTest, matrix_addition) {
     Matrix matA(2, 2);
     Matrix matB(2, 2);
