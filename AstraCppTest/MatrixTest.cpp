0d73e8d34838e35a576c3f6d4e27af93210e9a1d
saadthenexus <hasibulkarimsaad@gmail.com>
2024-11-13 14:17:44 +0600

    Added a test case for submatrix()

--- AstraCppTest/MatrixTest.cpp
+++ AstraCppTest/MatrixTest.cpp
@@ -1057,4 +1057,13 @@ TEST_F(MatrixTest, matrix_join_with_negative_values) {
     EXPECT_DOUBLE_EQ(matA(1, 3), -8.0);
 }
 
+TEST_F(MatrixTest, ValidSubmatrix) {
+    Matrix mat(4, 4, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+
+    Matrix submat = mat.submatrix(1, 1, 2, 2);
+    Matrix expected(2, 2, {6, 7, 10, 11});
+
+    EXPECT_EQ(submat, expected);
+}
+
 } // namespace astra
