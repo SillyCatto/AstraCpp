4dc6861759419cc44ac0125223a81adb26be1bae
Dr-Lepic <mbmahbub007@gmail.com>
2024-11-23 18:41:42 +0600

    implemented forward_sub and tested in demo file

--- AstraCpp/src/Solver.cpp
+++ AstraCpp/src/Solver.cpp
@@ -10,5 +10,19 @@ Vector Solver::forward_sub(Matrix L, Vector b) {
 	int m = b.get_size();
     Vector x(m);
 
-	// TODO
+	for (int v = 0; v < m; v++) {
+        if (L(v, v) == 0) {
+			// the diagonal is zero
+			x[v] = 0;
+            continue;
+		}	
+
+		double value = b[v];
+        for (int i = 0; i < v; i++) {
+			value -= L(v, i) * x[i];
+		}
+        x[v] = value / L(v, v);
+
+	}
+    return x;
 }
