d942dcf58a5b1a812d008bcbaeceb5455324fb1a
SillyCatto <raiyanmuhtasim@gmail.com>
2024-11-26 20:54:07 +0600

    made swap inline

--- AstraCpp/internals/Utils.h
+++ AstraCpp/internals/Utils.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace astra::internals::utils {
-    void swap(double& a, double& b) {
+    inline void swap(double& a, double& b) {
         double temp = a;
         a = b;
         b = temp;
