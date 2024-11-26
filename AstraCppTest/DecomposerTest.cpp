bbbcdfdaa8cc2042ffea7eed7b558caa88848e9c
SillyCatto <raiyanmuhtasim@gmail.com>
2024-11-26 18:35:53 +0600

    refactored test for decomposer

--- AstraCppTest/DecomposerTest.cpp
+++ AstraCppTest/DecomposerTest.cpp
@@ -19,7 +19,7 @@ class DecomposerTest : public ::testing::Test {
     void TearDown() override {}
 };
 
-TEST_F(DecomposerTest, HandlesSquareMatrix) {
+TEST_F(DecomposerTest, square_matrix) {
     
     Matrix mat(3, 3,
         { 2, 1, 1, 
@@ -29,18 +29,16 @@ TEST_F(DecomposerTest, HandlesSquareMatrix) {
     
     auto result = Decomposer::palu(mat);
 
-    EXPECT_EQ(result.P * result.L * result.U, mat)
-        << "P * L * U should reconstruct the original matrix.";
-    EXPECT_EQ(result.P.num_row(), result.P.num_col()) << "P must be square.";
-    EXPECT_EQ(result.L.num_row(), result.L.num_col()) << "L must be square.";
-    EXPECT_EQ(result.U.num_row(), result.U.num_col()) << "U must be square.";
-    EXPECT_TRUE(result.L.is_lower_triangular())
-        << "L must be a lower triangular matrix.";
-    EXPECT_TRUE(result.U.is_upper_triangular())
-        << "U must be an upper triangular matrix.";
+    EXPECT_EQ(result.P * result.L * result.U, mat);
+    EXPECT_EQ(result.P.num_row(), result.P.num_col());
+    EXPECT_EQ(result.L.num_row(), result.L.num_col());
+    EXPECT_EQ(result.U.num_row(), result.U.num_col());
+
+    EXPECT_TRUE(result.L.is_lower_triangular());
+    EXPECT_TRUE(result.U.is_upper_triangular());
 }
 
-TEST_F(DecomposerTest, HandlesNonInvertibleMatrix) {
+TEST_F(DecomposerTest, non_invertible_matrix) {
 
     Matrix mat(3, 3, 
         {1, 2, 3, 
@@ -49,10 +47,8 @@ TEST_F(DecomposerTest, HandlesNonInvertibleMatrix) {
 
     auto result = Decomposer::palu(mat);
 
-    EXPECT_EQ(result.U.principal_prod(), 0.0)
-        << "The determinant must be zero for singular "
-           "matrices.";
-    EXPECT_EQ(result.swaps % 2, 0) << "Swaps count parity is consistent.";
+    EXPECT_EQ(result.U.principal_prod(), 0.0);
+    EXPECT_EQ(result.swaps % 2, 0);
 }
 
 
