5f385906b96f784e69c2e78a27e0832e82c04a3a
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-08 14:09:35 +0600

    added 1 test for eqn solve, passed

--- AstraCppTest/SolverTest.cpp
+++ AstraCppTest/SolverTest.cpp
@@ -19,7 +19,20 @@ class SolverTest : public ::testing::Test {
     void TearDown() override {}
 };
 
+TEST_F(SolverTest, eqn_solve_test_1) { 
+    Matrix coeff_mat(4, 4, {1, 2, -1, 5,
+                            3, 6, -3, -2,
+                            7, -5, 3, -1,
+                            5, 10, 2, -7});
 
+
+    Vector constants{22, -2, 2, 3};
+
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{1, 2, 3, 4};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+}
 
 
 } // namespace astra
