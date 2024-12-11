3909501621b1755e92aeb3187e5a51e43507350d
Dr-Lepic <mbmahbub007@gmail.com>
2024-12-11 21:23:38 +0600

    wrote some test cases for solve but doesn't throw exception properly

--- AstraCppTest/SolverTest.cpp
+++ AstraCppTest/SolverTest.cpp
@@ -34,5 +34,81 @@ TEST_F(SolverTest, eqn_solve_test_1) {
     EXPECT_EQ(actual_ans, expected_ans);
 }
 
+TEST_F(SolverTest, eqn_solve_test_2) {
+    Matrix coeff_mat(3, 3, {1, 2, 3, 
+                            4, 5, 6, 
+                            7, 8, 10}
+    );
+
+    Vector constants{14, 32, 50};
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{-2, 8, 0};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+    
+}
+
+TEST_F(SolverTest, eqn_solve_test_3) {
+    Matrix coeff_mat(2, 2, {1, -1, 
+                            5, 3}
+    );
+
+    Vector constants{5, 9};
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{3, -2};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+}
+
+TEST_F(SolverTest, eqn_solve_test_4) {
+    Matrix coeff_mat(2, 2, {2, 4,
+                            4, -4}
+    );
+
+    Vector constants{14, 4};
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{3, 2};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+}
+
+TEST_F(SolverTest, eqn_solve_test_5_invalid) {
+    Matrix coeff_mat(2, 3, {1, 2, 3, 
+                            4, 9, 11}
+    );
+
+    Vector constants{5, 9};
+    EXPECT_THROW(Solver::solve(coeff_mat, constants),
+                 internals::exceptions::non_square_matrix);
+}
+
+TEST_F(SolverTest, eqn_solve_test_6_invalid) {
+    Matrix coeff_mat(3, 3, {1, 2, 3, 4, 5, 6, 7, 8, 10});
+
+    Vector constants{14, 32};
+    EXPECT_THROW(Solver::solve(coeff_mat, constants),
+                 internals::exceptions::variable_and_value_number_mismatch);
+}
+
+TEST_F(SolverTest, eqn_solve_test_7_singleton) {
+    Matrix coeff_mat(1, 1, {5});
+
+    Vector constants{10};
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{2};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+}
+
+TEST_F(SolverTest, eqn_solve_test_8_zero) {
+    Matrix coeff_mat(3, 3, {0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+    Vector constants{0, 0, 0};
+    Vector actual_ans = Solver::solve(coeff_mat, constants);
+    Vector expected_ans{0, 0, 0};
+
+    EXPECT_EQ(actual_ans, expected_ans);
+}
+
 
 } // namespace astra
